19c0eeb5a7fc2444cfe4d85256c3c8e072033d02
Nobuhiro Tabuki <japanese.around30@gmail.com>
2024-01-21 00:34:19 +0900

    user追加と取得をしてみる

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,27 +1,29 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::ops::Deref;
 use tauri::State;
 use crate::db_connection::db_connection::DbConnection;
+use crate::hash::hash::hash;
 use crate::users::users::{create_table, show_all};
 
 mod users;
 mod db_connection;
+mod hash;
+mod env;
 
 use crate::users::users::User;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(connection: State<DbConnection>, name: &str) -> String {
-    // let conn = Connection::open_in_memory().unwrap();
+
     let mutex_conn = connection.connection.lock().unwrap();
     let conn = mutex_conn.as_ref().unwrap();
     println!("{}", conn.is_autocommit());
 
     let _ = create_table(conn);
-    let test_result = User::add_new_user(conn, name.to_string(), name.to_string());
-
-    // let test_result = db_test(conn, name);
+    let password_hash = hash(name.to_string());
+    println!("password_hash: [{:?}]", password_hash);
+    let test_result = User::add(conn, name.to_string(), password_hash.0, password_hash.1);
 
     let _ = show_all(conn);
 
@@ -36,6 +38,15 @@ fn greet(connection: State<DbConnection>, name: &str) -> String {
 }
 
 fn main() {
+    fn show_dir(relative_path: &str) {
+        // Get current directory
+        let current_dir = std::env::current_dir().expect("Failed to get current directory");
+        let path = current_dir.join(relative_path);
+
+        println!("absolute path for [{}] is [{}]", relative_path, path.display())
+    }
+    show_dir(".env");
+
     tauri::Builder::default()
         .manage(DbConnection::create())
         .invoke_handler(tauri::generate_handler![greet])
--- src-tauri/src/users.rs
+++ src-tauri/src/users.rs
@@ -1,81 +1,88 @@
 pub mod users {
-    use rusqlite::{Connection, Result};
+    use rusqlite::{Connection, OptionalExtension, Result};
 
     #[derive(Debug)]
     pub struct User {
         user_id: i32,
         username: String,
-        // password_hash: String,
+    }
+
+    #[derive(Debug)]
+    pub struct UserAuthentication {
+        password_hash: String,
+        salt: String,
     }
 
     impl User {
-        pub fn add_new_user(conn: &Connection, username: String, password_hash: String) -> Result<User>  {
+        pub fn add(conn: &Connection, username: String, password_hash: String, salt: String) -> Result<User>  {
             let mut new_user = User {
                 user_id: 0,
                 username: username,
             };
             let inserted_row_cnt = conn.execute(
                 "INSERT INTO users \
-                    (username, password_hash)\
+                    (username, password_hash, salt)\
                  VALUES\
-                    (?1,?2)",
-                (&new_user.username, password_hash))?;
+                    (?1,?2, ?3)",
+                (&new_user.username, password_hash, salt))?;
             println!("insert return: {:?}", inserted_row_cnt);
 
             new_user.user_id = conn.last_insert_rowid() as i32;
             Ok(new_user)
         }
+
+        pub fn get(conn: &Connection, username: String) -> Result<Option<User>> {
+            let mut stmt = conn.prepare("\
+                        SELECT user_id, username, password_hash, salt \
+                        FROM users \
+                        WHERE username = ?1
+                        ")?;
+            let ret = stmt.query_row([username], |row| {
+                Ok(
+                    User {
+                    user_id: row.get(0)?,
+                    username: row.get(1)?,
+                    }
+                )
+            }).optional();
+            ret
+        }
     }
+
     pub fn create_table(connection: &Connection) -> Result<()> {
         connection.execute(
             "CREATE TABLE IF NOT EXISTS users (
                 user_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT NOT NULL,
-                password_hash TEXT
+                password_hash TEXT,
+                salt TEXT
             )",
             (), )?;
         Ok(())
     }
 
-    #[allow(dead_code)]
-    pub fn db_test(connection: &Connection, name: &str) -> Result<()> {
-        let me = User {
-            user_id: 0,
-            username: name.to_string(),
-            password_hash: "password".to_string(),
-        };
-        connection.execute("INSERT INTO users (username, password_hash) VALUES (?1,?2)",
-                           (&me.username, &me.password_hash),
-        )?;
-
-        let mut stmt = connection.prepare("SELECT user_id, username, password_hash FROM users")?;
-        let user_iter = stmt.query_map([], |row| {
-            Ok(User {
-                user_id: row.get(0)?,
-                username: row.get(1)?,
-                password_hash: row.get(2)?,
-            })
-        })?;
-
-        for user in user_iter {
-            println!("Found user {:?}", user.unwrap());
-        }
-        Ok(())
-    }
 
     #[allow(dead_code)]
     pub fn show_all(connection: &Connection) -> Result<()> {
-        let mut stmt = connection.prepare("SELECT user_id, username, password_hash FROM users")?;
+        let mut stmt = connection.prepare("SELECT user_id, username, password_hash, salt FROM users")?;
         let user_iter = stmt.query_map([], |row| {
-            Ok(User {
-                user_id: row.get(0)?,
-                username: row.get(1)?,
-                password_hash: row.get(2)?,
-            })
+            Ok(
+                (
+                    User {
+                        user_id: row.get(0)?,
+                        username: row.get(1)?,
+                    },
+                    UserAuthentication {
+                        password_hash: row.get(2)?,
+                        salt: row.get(3)?,
+                    }
+                )
+            )
         })?;
 
         for user in user_iter {
-            println!("Found user {:?}", user.unwrap());
+            let (u, ua) = user.unwrap();
+            println!("Found user {:?}, {:?}", u, ua);
         }
         Ok(())
     }
