edaf3d312490731e16d5517595b9fd9e141dc67d
Nobuhiro Tabuki <japanese.around30@gmail.com>
2024-04-13 21:54:55 +0900

    上部ボタン反応悪し

--- src/lib/LinkButton.svelte
+++ src/lib/LinkButton.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-export let url: string
-export let name: string
+  export let url: string
+  export let name: string
 </script>
 
-<button class="bg-green-700 hover:bg-green-600 text-white rounded px-4 py-1">
-  <a href={url}>{name}</a>
-</button>
+<a class="bg-green-700 hover:bg-green-600 cursor-pointer text-white rounded px-4 py-1"
+   href={url}>{name}</a>
